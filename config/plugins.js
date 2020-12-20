c37251f0a9fe250f5fb03d4bb9dffc7f28bdbaf2
Godwin Alexander <alecgee73@gmail.com>
2020-12-20 23:13:06 +0100

    still on aws-s3 oh

--- config/plugins.js
+++ config/plugins.js
@@ -2,8 +2,8 @@ module.exports = ({ env }) => ({
   upload: {
     provider: 'aws-s3',
     providerOptions: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_ACCESS_SECRET,
+      accessKeyId: env('AWS_ACCESS_KEY_ID') ,
+      secretAccessKey: env('AWS_ACCESS_SECRET'),
       region: 'US East (N. Virginia) us-east-1',
       params: {
         Bucket: 'nuxtstrapiecommerce',
