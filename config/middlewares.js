bffeaf30e55b0d7f6c7587d357ec57cd2955b90d
oviecodes <alecgee73@gmail.com>
2021-12-15 08:41:33 +0100

    update plugins for s3 upload

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,19 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'nuxtstrapiecommerce.s3.amazonaws.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'nuxtstrapiecommerce.s3.amazonaws.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
